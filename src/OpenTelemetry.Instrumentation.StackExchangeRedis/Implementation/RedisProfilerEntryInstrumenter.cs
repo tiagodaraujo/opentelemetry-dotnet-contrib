d8abc888f5242f70ed16d610ca6518c523832ece
Tiago Araujo <tiago.araujo@farfetch.com>
2024-08-16 19:02:00 +0100

    feat: add attr server.port if is not default 6379

--- src/OpenTelemetry.Instrumentation.StackExchangeRedis/Implementation/RedisProfilerEntryInstrumenter.cs
+++ src/OpenTelemetry.Instrumentation.StackExchangeRedis/Implementation/RedisProfilerEntryInstrumenter.cs
@@ -7,15 +7,18 @@ using System.Reflection;
 using System.Reflection.Emit;
 using OpenTelemetry.Trace;
 using StackExchange.Redis.Profiling;
+
 #if NET
 using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
-#endif
 
+#endif
 namespace OpenTelemetry.Instrumentation.StackExchangeRedis.Implementation;
 
 internal static class RedisProfilerEntryInstrumenter
 {
+    private const int DefaultPort = 6379;
+
     private static readonly Lazy<Func<object, (string?, string?)>> MessageDataGetter = new(() =>
     {
         Type profiledCommandType = Type.GetType("StackExchange.Redis.Profiling.ProfiledCommand, StackExchange.Redis", throwOnError: true)!;
@@ -159,9 +162,12 @@ internal static class RedisProfilerEntryInstrumenter
                 activity?.SetTag(SemanticConventions.AttributeNetPeerPort, port);
 
                 meterTags?.Add(SemanticConventions.AttributeServerAddress, ip);
-                meterTags?.Add(SemanticConventions.AttributeServerPort, port);
                 meterTags?.Add(SemanticConventions.AttributeNetworkPeerAddress, ip);
-                meterTags?.Add(SemanticConventions.AttributeNetworkPeerPort, port);
+                if (port != DefaultPort)
+                {
+                    meterTags?.Add(SemanticConventions.AttributeServerPort, port);
+                    meterTags?.Add(SemanticConventions.AttributeNetworkPeerPort, port);
+                }
             }
             else if (command.EndPoint is DnsEndPoint dnsEndPoint)
             {
@@ -172,7 +178,10 @@ internal static class RedisProfilerEntryInstrumenter
                 activity?.SetTag(SemanticConventions.AttributeNetPeerPort, port);
 
                 meterTags?.Add(SemanticConventions.AttributeServerAddress, host);
-                meterTags?.Add(SemanticConventions.AttributeServerPort, port);
+                if (port != DefaultPort)
+                {
+                    meterTags?.Add(SemanticConventions.AttributeServerPort, port);
+                }
             }
             else
             {
